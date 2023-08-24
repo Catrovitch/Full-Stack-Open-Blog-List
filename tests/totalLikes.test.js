4419af1b42101706ebe94944467fe48438f5d334
Catrovitch <ale.engelhardt@outlook.com>
2023-08-24 16:50:35 +0300

    4.4 done

---
+++ tests/totalLikes.test.js
@@ -0,0 +1,58 @@
+const listHelper = require('../utils/list_helper')
+
+const listWithOneBlog = [
+    {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+    }
+]
+const listWithManyBlogs = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'The Art of Computer Programming',
+        author: 'Donald E. Knuth',
+        url: 'http://www-cs-faculty.stanford.edu/~uno/taocp.html',
+        likes: 10,
+        __v: 1
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Introduction to Algorithms',
+        author: 'Thomas H. Cormen',
+        url: 'https://mitpress.mit.edu/books/introduction-algorithms',
+        likes: 7,
+        __v: 2
+    }
+];
+  
+  
+describe('Total likes', () => {
+    test('of empty list is zero', () => {
+      const blogs = []
+  
+      const result = listHelper.totalLikes(blogs)
+      expect(result).toBe(0)
+    })
+    
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('when list has many blogs, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithManyBlogs)
+        expect(result).toBe(22)
+    })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+function totalLikes(objects) {
+  const amountOfLikes = objects.reduce((accumulator, obj) => {
+    if (obj.hasOwnProperty('likes')) {
+      return accumulator + obj.likes;
+    }
+    return accumulator;
+  }, 0);
+
+  return amountOfLikes;
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
