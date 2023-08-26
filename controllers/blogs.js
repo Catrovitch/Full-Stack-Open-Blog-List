1252009d8df9ce836e54bde96406c576b7389c50
Catrovitch <ale.engelhardt@outlook.com>
2023-08-26 14:07:00 +0300

    4.18: bloglist expansion, step6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -11,14 +20,16 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
   
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'title and url are required' });
   }
 
-  const users = await User.find({})
-
-  const user = users[0]
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
