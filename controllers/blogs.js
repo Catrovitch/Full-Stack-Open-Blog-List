74695dbdac7cf405c0772424ea68150b4f70f241
Catrovitch <ale.engelhardt@outlook.com>
2023-08-27 12:54:29 +0300

    4.20* bloglist expansion step8 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,24 +3,11 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-    .populate('user', { username: 1, name: 1, id: 1 })
-
-  response.json(blogs);
-});
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
@@ -47,6 +34,14 @@ blogsRouter.post('/', async (request, response) => {
 
   response.status(201).json(savedBlog)
 })
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+    .populate('user', { username: 1, name: 1, id: 1 })
+
+  response.json(blogs);
+});
+
 
 blogsRouter.delete('/:id', async (request, response) => {
   try {
